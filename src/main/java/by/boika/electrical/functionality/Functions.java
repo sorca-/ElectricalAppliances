ec3cf64a13c0f336dd6039dfbba982ec1c8bec08
Dzmitry <sorca.by@gmail.com>
2014-06-18 16:19:59 +0300

    add some functions

---
+++ src/main/java/by/boika/electrical/functionality/Functions.java
@@ -0,0 +1,40 @@
+package by.boika.electrical.functionality;
+
+import by.boika.electrical.model.ElectricalAppliance;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import java.util.ArrayList;
+
+public class Functions {
+
+    private static final String LOG_CONFIGURATIONS_PATH = "src\\main\\resources\\log4j.xml";
+    static final Logger LOGGER = Logger.getLogger(Functions.class);
+    ArrayList<ElectricalAppliance> electricalAppliances;
+
+    static {
+        new DOMConfigurator().doConfigure(LOG_CONFIGURATIONS_PATH, LogManager.getLoggerRepository());
+    }
+
+    public Functions(ArrayList<ElectricalAppliance> electricalAppliances) {
+        this.electricalAppliances = electricalAppliances;
+    }
+
+    public void switchOnAllAppliance () {
+        for (ElectricalAppliance appliance : electricalAppliances) {
+            appliance.switchOn();
+        }
+        LOGGER.info("All appliances is work");
+    }
+
+    public int calculatePower() {
+        int sumPower = 0;
+        for (ElectricalAppliance appliance : electricalAppliances) {
+            if (appliance.isSwitchedOn()) {
+                sumPower+=appliance.getPower();
+            }
+        }
+        return sumPower;
+    }
+}
