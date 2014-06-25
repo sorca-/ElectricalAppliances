a9e00be56a5bd87a775f8490bd7ff90b84c87e9a
Dzmitry <sorca.by@gmail.com>
2014-06-26 00:29:02 +0300

    change main

--- src/main/java/by/boika/electrical/main/Main.java
+++ src/main/java/by/boika/electrical/main/Main.java
@@ -1,20 +1,17 @@
 package by.boika.electrical.main;
+
 import by.boika.electrical.exceptions.LogicalException;
 import by.boika.electrical.functionality.ControlAppliances;
 import by.boika.electrical.model.AbstractElectricalAppliance;
-import by.boika.electrical.model.Hairdryer;
 import by.boika.electrical.model.Home;
 import by.boika.electrical.parser.DOMParserReader;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.xml.sax.SAXException;
-import sun.rmi.runtime.Log;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
     private static final String LOG_CONFIGURATIONS_PATH = "src\\main\\resources\\log4j.xml";
@@ -35,8 +32,14 @@ public class Main {
         controlAppliances.switchOnAllAppliance(home.getIterator());
         LOGGER.info(controlAppliances.calculatePower(home.getIterator()));
         controlAppliances.switchOffAllAppliance(home.getIterator());
-        controlAppliances.findElectricalAppliance(home.getIterator(), "Hairdryer", 100).switchOn();
-        controlAppliances.findElectricalAppliance(home.getIterator(), "Hairdryer", 200).switchOn();
+        AbstractElectricalAppliance findAppliance = controlAppliances.findElectricalAppliance(home.getIterator(), "Hairdryer", 100);
+        if (findAppliance != null) {
+            findAppliance.switchOn();
+        }
+        findAppliance = controlAppliances.findElectricalAppliance(home.getIterator(), "Hairdryer", 200);
+        if (findAppliance != null) {
+            findAppliance.switchOn();
+        }
         LOGGER.info(controlAppliances.calculatePower(home.getIterator()));
     }
 }
--- src/main/java/by/boika/electrical/parser/DOMParserReader.java
+++ src/main/java/by/boika/electrical/parser/DOMParserReader.java
@@ -18,7 +18,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class DOMParserReader {
 
